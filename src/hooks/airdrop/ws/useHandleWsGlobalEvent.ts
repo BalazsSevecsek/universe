20f4a358f5d069e7973513e35c91278239419ba4
Balazs Sevecsek <mountain@tari.com>
2025-04-03 13:12:43 +0200

    fix: websocket-message formatting

--- src/hooks/airdrop/ws/useHandleWsGlobalEvent.ts
+++ src/hooks/airdrop/ws/useHandleWsGlobalEvent.ts
@@ -2,14 +2,13 @@ import { useAirdropStore } from '@app/store/useAirdropStore';
 import { WebsocketEventNames, WebsocketGlobalEvent } from '@app/types/ws';
 
 export const useHandleWsGlobalEvent = () => {
-    return (event: string) => {
-        const eventParsed = JSON.parse(event) as WebsocketGlobalEvent;
-        switch (eventParsed.name) {
+    return (event: WebsocketGlobalEvent) => {
+        switch (event.name) {
             case WebsocketEventNames.X_SPACE_EVENT:
-                useAirdropStore.setState({ latestXSpaceEvent: eventParsed.data });
+                useAirdropStore.setState({ latestXSpaceEvent: event.data });
                 break;
             default:
-                console.warn('Unknown global event', eventParsed);
+                console.warn('Unknown global event', event);
         }
     };
 };
